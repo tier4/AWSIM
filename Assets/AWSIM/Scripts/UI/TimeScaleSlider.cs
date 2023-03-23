f6880877d00c62d2aed3f08648ea7b2847f1941c
Dominik Jargot <dominik.jargot@robotec.ai>
2023-03-23 21:38:47 +0100

    Add an option for a user to define default time scale.

--- Assets/AWSIM/Scripts/UI/TimeScaleSlider.cs
+++ Assets/AWSIM/Scripts/UI/TimeScaleSlider.cs
@@ -1,17 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 namespace AWSIM
 {
+    [System.Serializable]
+    public class SimulationSettings
+    {
+        public float timeScale;
+    }
+
     public class TimeScaleSlider : MonoBehaviour
     {
         [SerializeField] Text timeScaleText;
         [SerializeField] Slider timeScaleSlider;
 
+        private TextAsset jsonFile;
+        private string filePath;
+
         private void Start()
         {
+            filePath = Application.dataPath + "/StreamingAssets/simulationSettings.json";
+            if (File.Exists(filePath))
+            {
+                string dataAsJson = File.ReadAllText(filePath);
+                SimulationSettings simSettings = JsonUtility.FromJson<SimulationSettings>(dataAsJson);
+                if (simSettings.timeScale < 0.0f)
+                {
+                    Debug.LogError("Time scale cannot be negative! Setting to 0.");
+                    simSettings.timeScale = 0.0f;
+                }
+                simSettings.timeScale = Math.Min(simSettings.timeScale, 1.0f);
+                timeScaleSlider.value = simSettings.timeScale;
+                Debug.Log($"Time scale set to: {simSettings.timeScale}");
+            }
+            Time.timeScale = timeScaleSlider.value;
             timeScaleText.text = "x " + timeScaleSlider.value.ToString("F2");
         }
 
