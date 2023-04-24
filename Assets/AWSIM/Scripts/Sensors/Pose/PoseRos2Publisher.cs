247ecc100838ba387f0caaf087fe70ca0619829d
mackierx111 <mackierx111@gmail.com>
2023-04-24 17:05:14 +0900

    add comments

--- Assets/AWSIM/Scripts/Sensors/Pose/PoseRos2Publisher.cs
+++ Assets/AWSIM/Scripts/Sensors/Pose/PoseRos2Publisher.cs
@@ -5,13 +5,25 @@ using ROS2;
 
 namespace AWSIM
 {
+    /// <summary>
+    /// Convert the data output from PoseSensor to ROS2 msg and Publish.
+    /// </summary>
     [RequireComponent(typeof(PoseRos2Publisher))]
     public class PoseRos2Publisher : MonoBehaviour
     {
+        /// <summary>
+        /// Topic name in pose msg.
+        /// </summary>
         public string Topic = "/awsim/ground_truth/vehicle/pose";
 
+        /// <summary>
+        /// Pose sensor frame id.
+        /// </summary>
         public string FrameID = "base_link";
 
+        /// <summary>
+        /// QoS settings.
+        /// </summary>
         public QoSSettings QosSettings = new QoSSettings()
         {
             ReliabilityPolicy = ReliabilityPolicy.QOS_POLICY_RELIABILITY_BEST_EFFORT,
@@ -27,10 +39,13 @@ namespace AWSIM
         // Start is called before the first frame update
         void Start()
         {
+            // Get GnssSensor component.
             sensor = GetComponent<PoseSensor>();
 
+            // Set callback.
             sensor.OnOutputData += Publish;
 
+            // Create msg.
             msg = new geometry_msgs.msg.PoseStamped()
             {
                 Header = new std_msgs.msg.Header()
@@ -40,14 +55,16 @@ namespace AWSIM
                 Pose = new geometry_msgs.msg.Pose(),
             };
 
+            // Create publisher.
             var qos = QosSettings.GetQoSProfile();
             poseStampedPublisher = SimulatorROS2Node.CreatePublisher<geometry_msgs.msg.PoseStamped>(Topic, qos);
         }
 
         void Publish(PoseSensor.OutputData outputData)
         {
-            var rosPosition = outputData.GroundTruthPosition;
-            var rosRotation = outputData.GrountTruthRotation;
+            // Converts data output from GnssSensor to ROS2 msg
+            var rosPosition = outputData.Position;
+            var rosRotation = outputData.Rotation;
 
             msg.Pose.Position.X = rosPosition.x;
             msg.Pose.Position.Y = rosPosition.y;
--- Assets/AWSIM/Scripts/Sensors/Pose/PoseSensor.cs
+++ Assets/AWSIM/Scripts/Sensors/Pose/PoseSensor.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 namespace AWSIM
 {
+    /// <summary>
+    /// Outputs the true value of pose.
+    /// TODO: Currently the Mgrs coordinate system is output, but refactor the Environment to Unity coordinate system + offset.
+    /// </summary>
     public class PoseSensor : MonoBehaviour
     {
+        /// <summary>
+        /// This data is output from PoseSensor at the OutputHz cycle.
+        /// </summary>
         public class OutputData
         {
-            public Vector3 GroundTruthPosition;
-            public Quaternion GrountTruthRotation;
+            public Vector3 Position;
+            public Quaternion Rotation;
+
+            public OutputData()
+            {
+                Position = new Vector3();
+                Rotation = new Quaternion();
+            }
         }
 
+        /// <summary>
+        /// Data output hz.
+        /// Sensor processing and callbacks are called in this hz.
+        /// </summary>
         [Range(0, 100)]
         public int OutputHz = 100;
 
+        /// <summary>
+        /// Delegate used in callbacks.
+        /// </summary>
         public delegate void OnOutputDataDelegate(OutputData outputData);
 
+        /// <summary>
+        /// Called each time data is output.
+        /// </summary>
         public OnOutputDataDelegate OnOutputData;
 
         float timer = 0;
@@ -40,11 +63,11 @@ namespace AWSIM
 
             // update ground truth position and rotation.
             var rosPosition = ROS2Utility.UnityToRosPosition(m_transform.position);
-            outputData.GroundTruthPosition = rosPosition + Environment.Instance.MgrsOffsetPosition;     // TODO: Handled in Unity coordinate system
-                                                                                                        // ros gnss sensor's pos + mgrs offset pos.
+            outputData.Position = rosPosition + Environment.Instance.MgrsOffsetPosition;     // TODO: Handled in Unity coordinate system
+                                                                                             // ros gnss sensor's pos + mgrs offset pos.
 
             var rosRotation = ROS2Utility.UnityToRosRotation(m_transform.rotation);
-            outputData.GrountTruthRotation = rosRotation;
+            outputData.Rotation = rosRotation;
             
              // Calls registered callbacks
             OnOutputData.Invoke(outputData);
