61ffba9bec56480fd56fa4f307dcc856471637fa
PiotrMrozik <piter012@vp.pl>
2023-07-25 11:34:06 +0200

    small fixes

--- Assets/RGLUnityPlugin/Scripts/LidarModels/LidarConfiguration.cs
+++ Assets/RGLUnityPlugin/Scripts/LidarModels/LidarConfiguration.cs
@@ -68,10 +68,13 @@ namespace RGLUnityPlugin
         [DrawIf("rayGenerateMethod", RayGenerateMethod.RotatingLidarEqualRange)] [Min(0)] public float maxRange;
 
         /// <summary>
-        /// The horiontal step time offset in milliseconds.
+        /// The vertical step time offset in milliseconds. Thisi s the time between two consecutive lasers in the array.
         /// </summary>
-        [Min(0)] public float horizontalStepTimeOffset;
         [Min(0)] public float verticalStepTimeOffset;
+
+        /// <summary>
+        /// Time offset between two consecutive firings of the whole array.
+        /// </summary>
         [Min(0)] public float rechargeTimeOffset;
 
         /// <summary>
