9d43d6485405c26ec7520c985326c6bafd0635b9
M. Fatih Cırıt <mfc@leodrive.ai>
2023-09-12 13:51:02 +0300

    feat: position reset with F5, F6, F7
    Signed-off-by: M. Fatih Cırıt <mfc@leodrive.ai>

--- Assets/AWSIM/Scripts/Vehicles/VehicleKeyboardInput.cs
+++ Assets/AWSIM/Scripts/Vehicles/VehicleKeyboardInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,6 +28,27 @@ namespace AWSIM
 
         [SerializeField] float maxAcceleration = 1.5f;
         [SerializeField] float maxSteerAngle = 35;
+
+        struct MyTransform
+        {
+            public Vector3 position;
+            public Quaternion rotation;
+        }
+        
+        private MyTransform originalTransform;
+        private MyTransform savedTransform;
+
+        private void Start()
+        {
+            if (vehicle == null)
+                vehicle = GetComponent<Vehicle>();
+            originalTransform = new MyTransform();
+            originalTransform.position = vehicle.transform.position;
+            originalTransform.rotation = vehicle.transform.rotation;
+            savedTransform = new MyTransform();
+            savedTransform.position = vehicle.transform.position;
+            savedTransform.rotation = vehicle.transform.rotation;
+        }
 
         void Reset()
         {
@@ -65,6 +87,26 @@ namespace AWSIM
                 vehicle.SignalInput = Vehicle.TurnSignal.HAZARD;
             else if (Input.GetKey(KeyCode.Alpha4))
                 vehicle.SignalInput = Vehicle.TurnSignal.NONE;
+
+            // Save the current transform when F7 is pressed
+            if (Input.GetKeyDown(KeyCode.F7))
+            {
+                savedTransform.position = vehicle.transform.position;
+                savedTransform.rotation = vehicle.transform.rotation;
+            }
+
+            // Set the vehicle's transform to the saved transform when F5 is pressed
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                vehicle.transform.position = originalTransform.position;
+                vehicle.transform.rotation = originalTransform.rotation;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                vehicle.transform.position = savedTransform.position;
+                vehicle.transform.rotation = savedTransform.rotation;
+            }
         }
     }
 }
