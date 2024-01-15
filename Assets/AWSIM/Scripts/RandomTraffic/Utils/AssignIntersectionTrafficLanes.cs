16f07948243331bc9a524304caa3dc1aa87368ce
Dawid Moszyński <dawid.moszynski@robotec.ai>
2024-01-15 12:38:29 +0100

    doc(assignIntersectionLane): add comments

--- Assets/AWSIM/Scripts/RandomTraffic/Utils/AssignIntersectionTrafficLanes.cs
+++ Assets/AWSIM/Scripts/RandomTraffic/Utils/AssignIntersectionTrafficLanes.cs
@@ -8,6 +8,11 @@ using ROS2;
 
 namespace AWSIM.TrafficSimulation
 {
+    /// <summary>
+    /// To make the yielding rules work properly, it is necessary to catagorize the TrafficLanes. 
+    /// The ones that belong to an intersection have the IntersectionLane variable set to true.
+    /// To automate the assignment of the corresponding IntersectionLane to each TrafficLane, this script can be used. 
+    /// </summary>
     [ExecuteInEditMode]
     public class AssignIntersectionTrafficLanes : MonoBehaviour
     {
@@ -29,14 +34,18 @@ namespace AWSIM.TrafficSimulation
 
         private void OnEnable()
         {
+            // checks if the reference to the parent of objects containing TrafficLanes has been assigned
             if (CheckReferences())
             {
+                // allows to remove current intersectionLane assignments in all children 
                 if (removeCurrentAssignment)
                     CleanUpIntersectionLanesVariable();
 
+                // allows to add incremented prefix ".<number" to names in all children, to have individual names
                 if (setNamesIncrementally)
                     SetTrafficLaneNames();
 
+                // allows to assign an IntersectionLane to a specific TrafficLane based on the RightOfWayLanes configuration of the others
                 if (assignBasedOnRightOfWayLanes)
                     AssignBasedOnRightOfWayLanes();
 
@@ -92,58 +101,63 @@ namespace AWSIM.TrafficSimulation
             Debug.Log("Previous command finished.");
         }
 
+
         private void AssignBasedOnRightOfWayLanes()
         {
             Debug.Log("Start assignment based on RightOfWayLanes.");
+            // iterate through all the children of trafficLanesObjectsParent
             foreach (Transform refTrafficLaneTransform in trafficLanesObjectsParent.transform)
             {
                 var refTrafficLane = refTrafficLaneTransform.GetComponent<TrafficLane>();
+                // check if the child has an TrafficLane script
                 if (refTrafficLane == null)
                 {
                     Debug.LogWarning($"Found GameObject ${refTrafficLaneTransform.name} without TrafficLane script in parent {trafficLanesObjectsParent.name}.");
                     continue;
                 }
+                // If refTrafficLane is already an intersectionLane, skip this TrafficLane
                 else if (refTrafficLane.intersectionLane == true)
                 {
-                    // If refTrafficLane is already an intersectionLane, skip this TrafficLane
                     continue;
                 }
+                // If refTrafficLane has RightOfWayLanes, set it as an intersectionLane and skip this TrafficLane
                 else if (refTrafficLane.RightOfWayLanes != null && refTrafficLane.RightOfWayLanes.Count > 0)
                 {
-                    // If refTrafficLane has RightOfWayLanes, set it as an intersectionLane and skip this TrafficLane
                     refTrafficLane.intersectionLane = true;
                     if (showDetailsOfEachAssignment)
                         Debug.Log($"{refTrafficLaneTransform.name} has been assigned as intersectionLane.");
                     continue;
                 }
-
-                // Search for otherTrafficLane that has refTrafficLane in RightOfWayLanes
-                foreach (Transform otherTrafficLaneTransform in trafficLanesObjectsParent.transform)
+                // Else search for otherTrafficLane that has refTrafficLane in RightOfWayLanes, if found, assign intersectionLane as true and break
+                else
                 {
-                    var otherTrafficLane = otherTrafficLaneTransform.GetComponent<TrafficLane>();
-                    if (otherTrafficLane == null)
+                    foreach (Transform otherTrafficLaneTransform in trafficLanesObjectsParent.transform)
                     {
-                        Debug.LogWarning($"Found GameObject ${otherTrafficLaneTransform.name} without TrafficLane script in parent {trafficLanesObjectsParent.name}.");
-                        continue;
-                    }
-
-                    foreach (TrafficLane otherRightOfLane in otherTrafficLane.RightOfWayLanes)
-                    {
-                        if (otherRightOfLane != null && otherRightOfLane.name == refTrafficLane.name)
+                        var otherTrafficLane = otherTrafficLaneTransform.GetComponent<TrafficLane>();
+                        if (otherTrafficLane == null)
                         {
-                            refTrafficLane.intersectionLane = true;
-                            otherTrafficLane.intersectionLane = true;
-                            if (showDetailsOfEachAssignment)
-                            {
-                                Debug.Log($"{refTrafficLaneTransform.name} has been assigned as intersectionLane.");
-                                Debug.Log($"{otherTrafficLaneTransform.name} has been assigned as intersectionLane.");
-                            }
-                            break;
+                            Debug.LogWarning($"Found GameObject ${otherTrafficLaneTransform.name} without TrafficLane script in parent {trafficLanesObjectsParent.name}.");
+                            continue;
                         }
+
+                        foreach (TrafficLane otherRightOfLane in otherTrafficLane.RightOfWayLanes)
+                        {
+                            if (otherRightOfLane != null && otherRightOfLane.name == refTrafficLane.name)
+                            {
+                                refTrafficLane.intersectionLane = true;
+                                otherTrafficLane.intersectionLane = true;
+                                if (showDetailsOfEachAssignment)
+                                {
+                                    Debug.Log($"{refTrafficLaneTransform.name} has been assigned as intersectionLane.");
+                                    Debug.Log($"{otherTrafficLaneTransform.name} has been assigned as intersectionLane.");
+                                }
+                                break;
+                            }
+                        }
+                        // If refTrafficLane is already an intersectionLane -> interrupt further search
+                        if (refTrafficLane.intersectionLane == true)
+                            break;
                     }
-                    // If refTrafficLane is already an intersectionLane -> interrupt further search
-                    if (refTrafficLane.intersectionLane == true)
-                        break;
                 }
             }
             Debug.Log("Previous command finished.");
