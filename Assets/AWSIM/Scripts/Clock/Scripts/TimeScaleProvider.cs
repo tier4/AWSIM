b8269c5a4301ce1f34914652447ac9f5f0e9ce98
Szymon Lis <szymonlis@yahoo.com>
2024-01-24 12:12:09 +0100

    Added thread-safe timescale provider

---
+++ Assets/AWSIM/Scripts/Clock/Scripts/TimeScaleProvider.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace AWSIM
+{
+    /// <summary>
+    /// A thread-safe static class to provide the value of the time scale of the simulation.
+    /// </summary>
+    public static class TimeScaleProvider
+    {
+        private static readonly object lockObject = new object();
+
+        private static float timeScale = 1.0f;
+        public static float TimeScale
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    return timeScale;
+                }
+            }
+        }
+
+
+        #region [Public Methods]
+
+        /// <summary>
+        /// Synchronise the value of the timeScale variable with the Time.timeScale from the Unity thread.
+        /// </summary>
+        public static void DoUpdate()
+        {
+            lock(lockObject)
+            {
+                if (Mathf.Abs(Time.timeScale - timeScale) > 0.01f)
+                {
+                    timeScale = Time.timeScale;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
+
