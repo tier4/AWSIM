9e236848cf6db84367b4ccbe97a0dc874a02fc24
mackierx111 <mackierx111@gmail.com>
2024-04-09 20:02:28 +0900

    add fps UI

--- Assets/AWSIM/Scripts/UI/DemoUI.cs
+++ Assets/AWSIM/Scripts/UI/DemoUI.cs
@@ -8,12 +8,36 @@ namespace AWSIM
     public class DemoUI : MonoBehaviour
     {
         [SerializeField] Text versionText;
+        [SerializeField] Text fpsText;
+
+        int frameCount;
+        float lastTime;
+        float fps;
 
         private void Start()
         {
             var version = Application.version;
             print(version);
             versionText.text = "AWSIM v " + version;
+
+            frameCount = 0;
+            lastTime = 0.0f;
+        }
+
+        private void Update()
+        {
+            frameCount++;
+            float time = Time.realtimeSinceStartup - lastTime;
+
+            if (time >= 0.5f)
+            {
+                fps = frameCount / time;
+
+                frameCount = 0;
+                lastTime = Time.realtimeSinceStartup;
+            }
+
+            fpsText.text = fps.ToString("F0") + " fps";
         }
     }
 }
