0042e906c44ddae4cd5ef1ec89b19b3b5c06a979
Szymon Lis <szymonlis@yahoo.com>
2024-06-05 21:03:32 +0200

    Added Lanelet Import Test

--- Assets/Tests/EditMode/Lanelet/LaneletTest.cs
+++ Assets/Tests/EditMode/Lanelet/LaneletTest.cs
@@ -11,55 +11,76 @@ using AWSIM;
 using AWSIM.Lanelet;
 using UnityEditor;
 
-
 public class LaneletTest
 {
     string sceneName = "Assets/Tests/EditMode/Lanelet/LaneletTest.unity";
     Scene scene;
-    AsyncOperation aOp;
-
 
     private Transform root = default;
-    //private LaneletTestUtils laneletTestUtils = default;
+    private Vector3EqualityComparer v3Comparer = new Vector3EqualityComparer(10e-6f);
 
-    private LaneletLoader.WaypointSettings waypointSettings = LaneletLoader.WaypointSettings.Default();
+
+    // --- TEST LIFE CYCLE ---//
 
     [UnitySetUp]
     public IEnumerator Setup()
     {
-        yield return LoadSceneAsync();
+        yield return LoadScene();
 
         root = AWSIM.Environment.Instance.transform;
         yield return null;
-        
     }
 
-    private IEnumerator LoadSceneAsync()
+    private IEnumerator LoadScene()
     {
         scene = EditorSceneManager.OpenScene(sceneName, OpenSceneMode.Single);
-
-        // Scene async operation
-        //aOp = EditorSceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-
-        //yield return new WaitUntil(() => aOp.isDone);
-        //scene = EditorSceneManager.GetSceneByName(sceneName);
         EditorSceneManager.SetActiveScene(scene);
         Assert.NotNull(scene);
 
         yield return null;
     }
 
-    [UnityTest]
-    public IEnumerator LaneletImporter_Main()
+    private void ImportLanelet()
     {
-        //string[] guids2 = AssetDatabase.FindAssets("lanelet_map_test", new[] {"Assets/Tests/EditMode/Lanelet"});
         OsmDataContainer osm = AssetDatabase.LoadAssetAtPath<OsmDataContainer>("Assets/Tests/EditMode/Lanelet/lanelet_map_test.osm");
-        Debug.Log(osm);
+        Assert.NotNull(osm);
 
         LaneletLoader loader = new LaneletLoader();
         loader.SetWaypointSettings(LaneletLoader.WaypointSettings.Default());
         loader.Load(osm.Data, AWSIM.Environment.Instance.MgrsOffsetPosition, 
-            root.gameObject);
+            root.gameObject);        
+    }
+
+    private void DestroyLanelet()
+    {
+        // remove imported stop lines
+        Transform trStops = root.Find("StopLines");
+        GameObject.DestroyImmediate(trStops.gameObject);
+
+        // remove imported traffic lanes
+        Transform trLanes = root.Find("TrafficLanes");
+        GameObject.DestroyImmediate(trLanes.gameObject);
+
+        // remove others left
+        List<Transform> toRemove = new List<Transform>();
+        for (int i=0; i<root.childCount; i++)
+        {
+            toRemove.Add(root.GetChild(i));
+        }
+
+        foreach(Transform tr in toRemove)
+        {
+            GameObject.DestroyImmediate(tr.gameObject);
+        }
+    }
+
+
+    // --- TEST ROUTINES --- //
+
+    [UnityTest]
+    public IEnumerator LaneletImporter_ObjectCount()
+    {
+        ImportLanelet();
 
         yield return null;
 
@@ -69,20 +90,123 @@ public class LaneletTest
         Transform trStops = root.Find("StopLines");
         Assert.NotNull(trStops);
 
+        // check the number of traffic lanes
         Assert.AreEqual(trLanes.childCount, 40);
 
+        // check the number of stop line
+        Assert.AreEqual(trStops.childCount, 4);
+
+        // clean
+        DestroyLanelet();
+    }
+
+    [UnityTest]
+    public IEnumerator LaneletImporter_TrafficLane_WaypointLenght()
+    {
+        ImportLanelet();
+
+        yield return null;
+
+        Transform trLanes = root.Find("TrafficLanes");
+        Assert.NotNull(trLanes);
+
+        // iterate over all traffic lanes
         for(int i=0; i<trLanes.childCount; i++)
         {
             TrafficLane lane = trLanes.GetChild(i).GetComponent<TrafficLane>();
             Assert.NotNull(lane);
-
-            foreach(TrafficLane tl in lane.NextLanes)
-            {
-                Debug.Log(Vector3.Distance(lane.Waypoints[lane.Waypoints.Length-1], tl.transform.position));
-            }
+            Assert.NotNull(lane.Waypoints);
+            Assert.GreaterOrEqual(lane.Waypoints.Length, 2);
         }
 
-        Assert.AreEqual(trStops.childCount, 4);
-
+        // clean
+        DestroyLanelet();
     }
+
+    [UnityTest]
+    public IEnumerator LaneletImporter_TrafficLane_LanePosition()
+    {
+        ImportLanelet();
+
+        yield return null;
+
+        Transform trLanes = root.Find("TrafficLanes");
+        Assert.NotNull(trLanes);
+
+        // iterate over all traffic lanes
+        for(int i=0; i<trLanes.childCount; i++)
+        {
+            TrafficLane lane = trLanes.GetChild(i).GetComponent<TrafficLane>();
+            Assert.NotNull(lane);
+            Assert.NotNull(lane.Waypoints);
+
+            // check if traffic lane object has the same position as first point in waypoints array
+            Assert.That(lane.transform.position, Is.EqualTo(lane.Waypoints[0]).Using(v3Comparer));
+        }
+
+        // clean
+        DestroyLanelet();
+    }
+
+    [UnityTest]
+    public IEnumerator LaneletImporter_NextLanePosition()
+    {
+        ImportLanelet();
+
+        yield return null;
+
+        Transform trLanes = root.Find("TrafficLanes");
+        Assert.NotNull(trLanes);
+
+        // iterate over all traffic lanes
+        for(int i=0; i<trLanes.childCount; i++)
+        {
+            TrafficLane lane = trLanes.GetChild(i).GetComponent<TrafficLane>();
+            Assert.NotNull(lane);
+            Assert.NotNull(lane.Waypoints);
+            Assert.GreaterOrEqual(lane.Waypoints.Length, 2);
+
+            Vector3 endPoint = lane.Waypoints[lane.Waypoints.Length-1];
+
+            // check if last waypoint position of current lane is equal to the first waypoint position of next lane
+            foreach(TrafficLane nextLane in lane.NextLanes)
+            {
+                Assert.NotNull(nextLane.Waypoints);
+                Assert.GreaterOrEqual(nextLane.Waypoints.Length, 2);
+
+                Assert.That(endPoint, Is.EqualTo(nextLane.Waypoints[0]).Using(v3Comparer));
+            }
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator LaneletImporter_PreviousLanePosition()
+    {
+        ImportLanelet();
+
+        yield return null;
+
+        Transform trLanes = root.Find("TrafficLanes");
+        Assert.NotNull(trLanes);
+
+        // iterate over all traffic lanes
+        for(int i=0; i<trLanes.childCount; i++)
+        {
+            TrafficLane lane = trLanes.GetChild(i).GetComponent<TrafficLane>();
+            Assert.NotNull(lane);
+            Assert.NotNull(lane.Waypoints);
+            Assert.GreaterOrEqual(lane.Waypoints.Length, 2);
+
+            // check if first waypoint position of current lane is equal to the last waypoint position of previous lane
+            foreach(TrafficLane prevLane in lane.PrevLanes)
+            {
+                Assert.NotNull(prevLane.Waypoints);
+                Assert.GreaterOrEqual(prevLane.Waypoints.Length, 2);
+
+                Vector3 endPoint = prevLane.Waypoints[prevLane.Waypoints.Length-1];
+                Assert.That(lane.Waypoints[0], Is.EqualTo(endPoint).Using(v3Comparer));
+            }
+        }
+    }
+
 }
