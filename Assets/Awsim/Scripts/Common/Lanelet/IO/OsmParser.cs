0a358025087613105c53d2b465519126b6fca69d
rokamu623 <ryo.okamura.3@tier4.jp>
2025-08-07 11:40:40 +0900

    fix: erase odd blank char
    Signed-off-by: rokamu623 <ryo.okamura.3@tier4.jp>

--- Assets/Awsim/Scripts/Common/Lanelet/IO/OsmParser.cs
+++ Assets/Awsim/Scripts/Common/Lanelet/IO/OsmParser.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-﻿using System.Linq;
+using System.Linq;
 using System.Xml;
 
 namespace Awsim.Common
